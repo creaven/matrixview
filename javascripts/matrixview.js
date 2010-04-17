697b9927fd427c746aed33f25a264b01ae51f70e
creaven <creavenmoro@gmail.com>
2010-04-17 11:03:27 +0400

    more refactoring

--- javascripts/matrixview.js
+++ javascripts/matrixview.js
@@ -33,28 +33,34 @@ var MatrixView = new Class({
 	},
 
 	events: function() {
-		this.bound('keypress', 'dblclick', 'mousedown', 'mouseup', 'mousemove');
+		this.bound('keypress', 'dblclick', 'mousedown', 'mouseup', 'mousemove', 'stopSelection');
 		this.element.addEvents({
 			dblclick: this.bound.dblclick,
-			mousedown: this.bound.mousedown,
+			mousedown: this.bound.mousedown
+		});
+		document.addEvents({
 			mouseup: this.bound.mouseup,
 			mousemove: this.bound.mousemove
 		});
-		document.addEvent('keypress', this.bound.keypress);
-
+		var keypress = (Browser.Engine.trident || Browser.Engine.webkit) ? 'keydown' : 'keypress';
+		document.addEvent(keypress, this.bound.keypress);
+	},
+	
+	stopSelection: function(event){
+		event.preventDefault();
 	},
 
 	keypress: function(event) {
 		if (event.meta && (event.keyCode == 97 || event.keyCode == 65)) {
 			this.selectAll();
-			event.stop();
+			event.preventDefault();
 			return null;
 		} else if (event.shift) {
 			if (['left', 'right', 'top', 'bottom'].contains(event.key)) {
 				this['expandSelection' + event.key.capitalize()](event);
 			}
 			if (event.key == 'space') {
-				event.stop();
+				event.preventDefault();
 			}
 			if (event.key == 'tab' && this.selectedItems.length) {
 				this.moveLeft(event);
@@ -69,16 +75,16 @@ var MatrixView = new Class({
 		}
 		if (['delete', 'backspace'].contains(event.key)) {
 			this.destroy(this.selectedItems);
-			event.stop();
+			event.preventDefault();
 		}
 		if (['left', 'right', 'down', 'up'].contains(event.key)) {
 			this['move' + event.key.capitalize()](event);
 		}
 		if (event.key == 'space') {
-			event.stop();
+			event.preventDefault();
 		}
 		if (event.key == 'tab' && this.selectedItems.length) {
-			this.moveRigth(event);
+			this.moveRight(event);
 		}
 		return null;
 	},
@@ -94,6 +100,7 @@ var MatrixView = new Class({
 
 	mousedown: function(event) {
 		event.preventDefault();
+		window.focus();
 		var element = document.id(event.target);
 		element = element.getAncestor('li');
 		if (element) {
@@ -109,16 +116,17 @@ var MatrixView = new Class({
 			left: event.page.x - this.element.getPosition().x,
 			top: event.page.y - this.element.getPosition().y
 		});
+		document.addEvent('selectstart', this.bound.stopSelection);
 	},
 
 	mouseup: function(event) {
-		event.stop();
 		this.dragging = false;
 		this.selectionArea.setStyles({
 			width: 0,
 			height: 0,
 			display: 'none'
 		});
+		document.removeEvent('selectstart', this.bound.stopSelection);
 		this.fireEvent('select', this.selectedItems);
 	},
 
@@ -151,29 +159,18 @@ var MatrixView = new Class({
 			width: width,
 			height: height
 		});
-		function intersect(a, b, flag){
-			var sides = [[0, 1, 0, 3], [0, 1, 2, 1], [0, 3, 2, 3], [2, 1, 2, 3]];
-			for(var i = 0; i < 4; i++){
-				var side = sides[i];
-				var x1 = a[side[0]];
-				var y1 = a[side[1]];
-				var x2 = a[side[2]];
-				var y2 = a[side[3]];
-				var left = b[0];
-				var top = b[1];
-				var right = b[2];
-				var bottom = b[3];
-				if( left <= x1 && x1 <= right && top <= y1 && y1 <= bottom ) return true;
-				if( left <= x2 && x2 <= right && top <= y2 && y2 <= bottom ) return true;
-				if( x1 <= left && x2 >= left && top <= y2 && y2 <= bottom) return true;
-				if( y1 <= top && y2 >= top && left <= x2 && x2 <= right) return true;
-			}
-			if(flag) return false;
-			return intersect(b, a, true);
-		}
+		function intersect(a, b){
+			var left = Math.max(a.left, b.left);
+			var right = Math.min(a.right, b.right);
+			var top = Math.max(a.top, b.top);
+			var bottom = Math.min(a.bottom, b.bottom);
+			return left <= right && top <= bottom ? true : false;
+		};
 		this.element.getElements('li').each(function(element) {
 			var coords = element.getCoordinates();
-			if (intersect([left, top, right, bottom], [coords.left, coords.top, coords.right, coords.bottom])){
+			coords.top += this.element.scrollTop;
+			coords.bottom += this.element.scrollTop;
+			if (intersect(coords, {left: left, right: right, top: top, bottom: bottom})){
 				element.addClass('selected');
 				this.selectedItems.include(element);
 			} else {
@@ -250,7 +247,7 @@ var MatrixView = new Class({
 	},
 
 	selectFirst: function() {
-		var element = this.element.getFirst('li');
+		var element = this.element.getElement('li');
 		this.deselectAll();
 		this.select(element);
 		this.scrollIntoView(element, 'down');
@@ -258,7 +255,7 @@ var MatrixView = new Class({
 	},
 
 	selectLast: function() {
-		var element = this.element.getElement('li:list-child');
+		var element = this.element.getElement('li:last-child');
 		this.deselectAll();
 		this.select(element);
 		this.scrollIntoView(element, 'down');
@@ -266,93 +263,97 @@ var MatrixView = new Class({
 	},
 
 	moveLeft: function(event) {
-		event.stop();
-		var element = this.element.getElement('li.selected');
-		if (!element) return this.selectFirst();
+		event.preventDefault();
+		var element = this.element.getElements('li.selected').getLast();
+		if (!element) return this.selectLast();
 		var previousElement = element.getPrevious();
 		if (previousElement) {
 			this.select(previousElement);
 			this.scrollIntoView(previousElement, 'up');
-		} else {
-			this.selectFirst();
 		}
 		return null;
 	},
 
 	moveRight: function(event) {
-		event.stop();
-		var element = this.element.getElement('li.selected:last-child');
-		if (!element) {
-			return this.selectFirst();
-		}
+		event.preventDefault();
+		var element = this.element.getElement('li.selected');
+		if (!element) return this.selectFirst();
 		var next = element.getNext();
 		if (next) {
 			this.select(next);
 			this.scrollIntoView(next, 'down');
-		} else {
-			this.selectLast();
 		}
 		return null;
 	},
 
 	moveUp: function(event) {
-		event.stop();
+		event.preventDefault();
 		var element = this.element.getElement('li.selected');
-		if (!element) return this.selectFirst();
+		if (!element) return this.selectLast();
 		var offset = element.getPosition();
 		var y = Math.floor(offset.y - element.offsetHeight);
-
-		var previous = element.getAllPrevious();
-		if (!previous.length) return this.selectFirst();
-
-		previous.each(function(el) {
-			if (Position.within(el, offset.x, y)) {
-				this.select(el);
-				this.scrollIntoView(el, 'up');
+		
+		function inside(element, x, y){
+			var coords = element.getCoordinates();
+			return (coords.left <= x && x <= coords.right && coords.top <= y && y <= coords.bottom) ? true : false;
+		};
+		var previous = element.getPrevious();
+		while(previous){
+			if(inside(previous, offset.x, y)){
+				this.select(previous);
+				this.scrollIntoView(previous);
+				break;
 			}
-		});
+			previous = previous.getPrevious();
+		};
 		return null;
 	},
 
 	moveDown: function(event) {
-		event.stop();
-		var element = this.element.getElement('li.selected:last-child');
+		event.preventDefault();
+		var element = this.element.getElements('li.selected').getLast();
 		if (!element) return this.selectFirst();
 		var offset = element.getPosition();
 		var y = Math.floor(offset.y + element.offsetHeight + (element.offsetHeight / 2)) + parseInt(element.getStyle('margin-bottom'), 10);
 
-		var next = element.getAllNext();
-		if (!next) return this.selectLast();
-		var selected = false;
-		next.each(function(el) {
-			if (Position.within(el, offset[0], y)) {
-				this.select(el);
-				this.scrollIntoView(el, 'down');
-				selected = true;
+		function inside(element, x, y){
+			var coords = element.getCoordinates();
+			return (coords.left <= x && x <= coords.right && coords.top <= y && y <= coords.bottom) ? true : false;
+		};
+		var next = element.getNext();
+		while(next){
+			if(inside(next, offset.x, y)){
+				this.select(next);
+				this.scrollIntoView(next);
+				break;
 			}
-		});
-		if (!selected) this.selectLast();
+			next = next.getNext();
+		};
 		return null;
 	},
 
 	expandSelectionLeft: function(event) {
 		var element = this.element.getElement('li.selected');
-		var otherElement = element.getPrevious().addClass('selected');
-		this.selectedItems.push(otherElement);
-		this.scrollIntoView(element, 'up');
+		var previous = element.getPrevious();
+		if(!previous) return;
+		previous.addClass('selected');
+		this.selectedItems.push(previous);
+		this.scrollIntoView(element);
 		this.fireEvent('select', [element]);
 	},
 
 	expandSelectionRight: function(event) {
-		var element = this.element.getElement('li.selected');
-		var otherElement = element.getNext().addClass('selected');
-		this.selectedItems.push(otherElement);
-		this.scrollIntoView(element, 'down');
+		var element = this.element.getElements('li.selected').getLast();
+		var next = element.getNext();
+		if(!next) return;
+		next.addClass('selected');
+		this.selectedItems.push(next);
+		this.scrollIntoView(element);
 		this.fireEvent('select', [element]);
 	},
 
 	expandSelectionUp: function(event) {
-		event.stop();
+		event.preventDefault();
 		var element = this.element.down('li.selected');
 		var itemWidth = element.getWidth();
 		var itemOffset = Position.cumulativeOffset(element);
@@ -371,12 +372,12 @@ var MatrixView = new Class({
 	},
 
 	expandSelectionDown: function(event) {
-		event.stop();
+		event.preventDefault();
 		var element = this.element.getElement('li.selected:last-child');
 		var offset = element.getPosition();
 		var y = Math.floor(offset.y + element.offsetHeight + (element.offsetHeight / 2)) + parseInt(element.getStyle('margin-bottom'), 10);
 		var done = false;
-		element.getAllNext.each(function(el) {
+		element.getAllNext().each(function(el) {
 			if (done == false) {
 				el.addClass('selected');
 				this.selectedItems.push(el);
@@ -393,16 +394,21 @@ var MatrixView = new Class({
 		return this.element.getElements('li');
 	},
 
-	scrollIntoView: function(element, direction) {
-		var scrollingView = this.element;
-		if (direction == 'down' || direction == 'right') {
-			if (((Position.page(element)[1] + element.offsetHeight) >= (this.element.offsetHeight + this.element.getPosition().y)) || (Position.page(element)[1] <= 0)) {
-				this.element.scrollTop = element.getPosition().y - this.element.offsetHeight + element.offsetHeight;
-			}
-		} else if (direction == 'up' || direction == 'left') {
-			if (((Position.page(element)[1] + element.getHeight()) >= (scrollingView.getHeight() + Position.cumulativeOffset(scrollingView)[1])) || (Position.page(element)[1] <= 0)) {
-				this.element.scrollTop = element.getPosition().y - parseInt(element.getStyle('margin-top'), 10) - 24;
-			}
+	scrollIntoView: function(element) {
+		var top = element.offsetTop;
+		var bottom = top + element.offsetHeight;
+		if(top - this.element.scrollTop < 0) {
+			this.element.scrollTop = top;
+		} else if(this.element.scrollTop + this.element.offsetHeight - bottom < 0){
+			this.element.scrollTop = bottom - this.element.offsetHeight;
+		}
+		
+		var left = element.offsetLeft;
+		var right = left + element.offsetWidth;
+		if(left - this.element.scrollLeft < 0) {
+			this.element.scrollLeft = left;
+		} else if(this.element.scrollLeft + this.element.offsetWidth - right < 0){
+			this.element.scrollLeft = right - this.element.offsetWidth;
 		}
 	}
 
